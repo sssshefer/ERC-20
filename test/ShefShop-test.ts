94fce715359f078597362b55c900a4c8d146e74d
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-07-05 22:24:17 +0300

    allows to buy and sell tests

--- test/ShefShop-test.ts
+++ test/ShefShop-test.ts
@@ -18,7 +18,51 @@ describe("AuctionEngine", function () {
         expect(await shefShop.owner()).to.eq(owner.address);
         expect (await shefShop.token()).to.be.properAddress;       
     })
+    
+    it("allows to buy", async function() {
+        const { owner,buyer, shefShop, erc20 } = await loadFixture(deploy);
+        const tokenAmount = 3;
+        const txData = {
+            value:tokenAmount,
+            to:shefShop
+        }
 
-    
-    
+        const tx = await buyer.sendTransaction(txData);
+        await tx.wait();
+
+        expect(await erc20.balanceOf(buyer.address)).to.eq(tokenAmount);
+
+        await expect(()=> tx).to.changeEtherBalance(shefShop, tokenAmount);
+
+        await expect(tx).to.emit(shefShop, "Bought")
+        .withArgs(tokenAmount, buyer.address)
+    })
+
+    it("allows to sell", async function() {
+        const { owner,buyer, shefShop, erc20 } = await loadFixture(deploy);
+
+        const tokensToBuy = 3;
+        const tx = await buyer.sendTransaction({
+            value:tokensToBuy,
+            to:shefShop
+        })
+
+        await tx.wait()
+
+        const sellAmount = 2;
+        
+        const approval = await erc20.connect(buyer).approve(shefShop, sellAmount);
+
+        await approval.wait();
+
+        const sellTx =  await shefShop.connect(buyer).sell(sellAmount);
+        
+        expect(await erc20.balanceOf(buyer.address)).to.eq(1);
+
+        await expect(()=> sellTx).to.changeEtherBalance(shefShop, -sellAmount);
+
+        await expect(sellTx).to.emit(shefShop, "Sold")
+        .withArgs(sellAmount, buyer.address)
+
+    })   
 });
